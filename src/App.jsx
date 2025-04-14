5c9460e042567d4a29fa6ab77128ea720a4d6440
ZeinAllame <zeinallame006@gmail.com>
2025-04-15 00:51:48 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-pink-100 text-2xl font-bold">
+      أهلاً بك في Lilo Fashion
+    </div>
+  )
+}
+
+export default App
