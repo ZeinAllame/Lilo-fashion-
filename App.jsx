256d0a9a87a661a8cc743cda32ec9a47dca551df
ZeinAllame <zeinallame006@gmail.com>
2025-04-14 23:43:21 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="text-center p-10 text-2xl">
+      مرحباً بك في موقع Lilo Fashion! سيتم إدراج الصفحات هنا لاحقاً.
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Lilo Fashion Website
+This is a React + Tailwind CSS project for Lilo Fashion.
