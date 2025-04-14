8b406ac7f4ff2ae8f87ffa5c08b06a6a5a52ce1c
ZeinAllame <zeinallame006@gmail.com>
2025-04-15 00:20:51 +0300

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <h1>Lilo Fashion</h1>
   </React.StrictMode>
-);
+)
