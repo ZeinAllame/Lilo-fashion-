37dfe5288e10571cf75e409665fc261ab439f6a2
ZeinAllame <zeinallame006@gmail.com>
2025-04-17 23:30:20 +0300

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-pink-100 text-2xl font-bold">
-      أهلاً بك في Lilo Fashion
-    </div>
-  )
-}
-
-export default App
